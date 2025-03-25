95d1dab92122ddbbc2a37f07fd57678bfd75f28f
sheilloputratama <sheilloggendut@gmail.com>
2025-03-25 19:16:15 +0700

    menambah fungsi insertionsort unutk mengurutkan array

--- contoh_insertionsort.cpp
+++ contoh_insertionsort.cpp
@@ -32,4 +32,30 @@ cin >> arr[i]; //menyimpan nilai data n kedalam array arr
 }
 }
 
-void
+void insertionsort()
+{//procedure insertionsort
+
+int temp; //membuat variable data temporer atau penyimpanan sementara
+int j, i; //membuat variable j sebagai penanda
+
+for (i = 1; i <= n - 1; i++)
+{//step 1
+
+temp = arr[i]; //step 2
+
+j = i - 1; //step 3
+
+while (j >= 0 && arr[j] > temp) // step 4
+{
+arr[j + 1] = arr[j]; //step 4a
+j--; //step 4b
+}
+
+arr[j + 1] = temp; //step 5
+}
+}
+
+void display()
+{
+    
+}
